8b1f995463e6a5ff14ff128e865537f397ee7b76
danigirol <danigirol@gmail.com>
2025-01-15 12:10:50 +0100

    Posicionamiento web

---
+++ girolconsulting/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Girol Consulting - Posicionamiento web en Barcelona
+ * Plugin URI: https://girol.es/posicionamiento-web-en-barcelona/
+ * Description: Este es un plugin básico de ejemplo para WordPress.
+ * Version: 1.0
+ * Author: Dani Girol
+ * Author URI: https://posicionamientowebenbarcelona.com
+ * License: GPL2
+ */
+
+ ?>
